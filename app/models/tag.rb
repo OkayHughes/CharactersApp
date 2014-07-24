595e356dba5b981e0ba709ca3c550b433f6332d8
Caleb Jones <bionicle.dragon@gmail.com>
2014-07-24 16:09:46 +0000

    Made edits to tag change character updated_at date Added therubyracer so that EC2 can run JS

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ActiveRecord::Base
-	belongs_to :character
+	belongs_to :character, touch: true
 	#optional uniqueness within character uniqueness: {scope: :character},
 	validates :key, presence: true
 	validates :value, presence: true
