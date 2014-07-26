fa03caaaeb505818321a1494173017af016b2141
OkayHughes <OkayHughes@gmail.com>
2014-07-26 00:31:21 -0400

    Fixes #19 Refactored cookies into helper method. Made create change cookies.

--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -1,2 +1,10 @@
 module CharactersHelper
+	def current_character
+		Character.find(cookies.permanent[:character_id])
+	end
+
+	def set_current_character(id)
+		cookies.delete(:character_id)
+		cookies.permanent[:character_id] = id
+	end
 end
