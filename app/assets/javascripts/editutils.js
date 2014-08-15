93ff24f91f4203aed3fb91a2ffc361e1262f64eb
OkayHughes <OkayHughes@gmail.com>
2014-08-14 23:41:49 -0400

    Creates automatic ability filling buttons. Fixes #38

--- app/assets/javascripts/editutils.js
+++ app/assets/javascripts/editutils.js
@@ -27,12 +27,31 @@ function cancelNewCharacter(){
 	$(".new_character").html('');
     $(".background").css("opacity", "1");
 }
-
-function toggleNewSigmas(){
-    $("#new_sigmas").toggle();
-}
-function toggleSigmas(){
-    $("#sigmas").toggle();
+//random integer between [lo,high]
+function randInt(hi, lo){
+    return Math.floor((Math.random() * (hi-lo)) + 0.999) + lo;
 }
 
+function assignRandomByShuffle(){
 
+    var shuffleDistribution = [16, 14, 13, 12, 11, 10];
+    var fieldIds = ["#strength", "#dexterity", "#constitution", "#intelligence", "#wisdom", "#charisma"];
+    for (var i = 0; i < 6; i++){
+        var rand = randInt(0, fieldIds.length - 1);
+
+        $(fieldIds[rand]).val(shuffleDistribution[i]);
+        fieldIds.splice(rand, 1);
+    }
+    $("#health").val(randInt(8, 14) + parseInt($("#constitution").val()));
+}
+
+function assignRandomByDice(){
+    var fieldIds = ["#strength", "#dexterity", "#constitution", "#intelligence", "#wisdom", "#charisma"];
+    var rands = [];
+    for (var i=0; i < 6; i++){
+        rands = [randInt(1, 6), randInt(1, 6), randInt(0, 6), randInt(0, 6)];
+        rands.splice(rands.indexOf(Math.min.apply(Math, rands)), 1);
+        $(fieldIds[i]).val(rands.reduce(function(prev, cur){ return prev + cur;}));
+    }
+    $("#health").val(randInt(8, 14) + parseInt($("#constitution").val()));
+}
