977312e7e8a87b4ec48ef6678a2f042717def1b8
OkayHughes <OkayHughes@gmail.com>
2014-08-15 00:08:41 -0400

    Put buttons into new form too. Oops.

--- app/assets/javascripts/editutils.js
+++ app/assets/javascripts/editutils.js
@@ -32,26 +32,43 @@ function randInt(hi, lo){
     return Math.floor((Math.random() * (hi-lo)) + 0.999) + lo;
 }
 
-function assignRandomByShuffle(){
-
+function assignRandomByShuffle(new_fields){
     var shuffleDistribution = [16, 14, 13, 12, 11, 10];
-    var fieldIds = ["#strength", "#dexterity", "#constitution", "#intelligence", "#wisdom", "#charisma"];
+    if (new_fields){
+        var fieldIds = ["#new_strength", "#new_dexterity", "#new_constitution", "#new_intelligence", "#new_wisdom", "#new_charisma"];
+    } else {
+        var fieldIds = ["#strength", "#dexterity", "#constitution", "#intelligence", "#wisdom", "#charisma"];
+    }
     for (var i = 0; i < 6; i++){
         var rand = randInt(0, fieldIds.length - 1);
 
         $(fieldIds[rand]).val(shuffleDistribution[i]);
         fieldIds.splice(rand, 1);
     }
-    $("#health").val(randInt(8, 14) + parseInt($("#constitution").val()));
+    if (new_fields){    
+        $("#new_health").val(randInt(8, 14) + parseInt($("#new_constitution").val()));
+    } else {
+        $("#health").val(randInt(8, 14) + parseInt($("#constitution").val()));
+    }
 }
 
-function assignRandomByDice(){
-    var fieldIds = ["#strength", "#dexterity", "#constitution", "#intelligence", "#wisdom", "#charisma"];
+function assignRandomByDice(new_fields){
+    if (new_fields){
+        var fieldIds = ["#new_strength", "#new_dexterity", "#new_constitution", "#new_intelligence", "#new_wisdom", "#new_charisma"];
+    } else {
+        var fieldIds = ["#strength", "#dexterity", "#constitution", "#intelligence", "#wisdom", "#charisma"];
+    }    
     var rands = [];
     for (var i=0; i < 6; i++){
         rands = [randInt(1, 6), randInt(1, 6), randInt(0, 6), randInt(0, 6)];
         rands.splice(rands.indexOf(Math.min.apply(Math, rands)), 1);
         $(fieldIds[i]).val(rands.reduce(function(prev, cur){ return prev + cur;}));
     }
-    $("#health").val(randInt(8, 14) + parseInt($("#constitution").val()));
+    if (new_fields){    
+        $("#new_health").val(randInt(8, 14) + parseInt($("#new_constitution").val()));
+    } else {
+        $("#health").val(randInt(8, 14) + parseInt($("#constitution").val()));
+    }
 }
+
+
