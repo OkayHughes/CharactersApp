526cafd5aa5bea30e81369bdc108bd0f863d7a74
OkayHughes <OkayHughes@gmail.com>
2014-08-14 22:25:40 -0400

    Created key-pair uniqueness validation within characters. Fixes #40

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,5 +3,12 @@ class Tag < ActiveRecord::Base
 	#optional uniqueness within character uniqueness: {scope: :character},
 	validates :key, presence: true
 	validates :value, presence: true
+	validate :no_key_value_pair_duplicates
 
+	def no_key_value_pair_duplicates()
+		t = Character.find(character_id).tags
+		if t.where(key: key, value: value).length != 0
+			errors.add(:value, "key pairs cannot be duplicates")
+		end
+	end
 end
